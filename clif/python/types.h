5b46bdcd461f96ca92f75217550b824c9c63b2fc
akekawit <akekawit@google.com>
2021-01-07 20:15:40 +0000

    Remove unused "base/commandlineflags.h" include
    The include seems to be unused and ends up causing a dependency on gflags on the opensource version.
    
    PiperOrigin-RevId: 350613539

--- clif/python/types.h
+++ clif/python/types.h
@@ -44,7 +44,6 @@ headers are included.
 #include <utility>
 #include <vector>
 
-#include "base/commandlineflags.h"
 #include "glog/logging.h"
 #include "absl/base/config.h"
 #include "absl/numeric/int128.h"
